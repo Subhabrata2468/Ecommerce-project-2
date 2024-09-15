bea8637b3000c6e38da832be1be8601f20db4ce1
subhabrata2468 <panda.subhabrata2003@gmail.com>
2024-09-15 18:24:01 +0530

    Modification of snappy-apply file

--- K8s/snappy-apply.sh
+++ K8s/snappy-apply.sh
@@ -6,18 +6,20 @@
 #kubectl delete pods,services,deployments,horizontalpodautoscaler --all -n shopping 
 
 # Apply MongoDB deployment and service
-kubectl apply -f persistentVolumeClaim.yml
 kubectl apply -f storageClass-for-mongos.yml
+kubectl apply -f persistentVolumeClaim.yml
 kubectl apply -f mongo-deployment.yml
 kubectl apply -f mongo-service.yml
 kubectl apply -f hpa-mongo.yml
 
 # Apply Backend deployment and service
+kubectl apply -f backend-configmap.yml
 kubectl apply -f backend-deployment.yml
 kubectl apply -f backend-service.yml
 kubectl apply -f hpa-backend.yml
 
 # Apply Frontend deployment and service
+kubectl apply -f frontend-configmap.yml
 kubectl apply -f frontend-deployment.yml
 kubectl apply -f frontend-service.yml
 kubectl apply -f hpa-frontend.yml
